1b57cc15454bb6c764650b1f26ca549fda3aa1ea
lin <88322511@qq.com>
2019-07-19 11:51:20 +0800

    comment01

--- 2019/July/lesson01/hello_01.py
+++ 2019/July/lesson01/hello_01.py
@@ -1,3 +1,15 @@
 #enconding: utf-8
 
-print('hello world');
+print('hello world')
+
+## enconding: utf-8
+## use 4 spaces instead of tab key
+## newline user "\n"
+
+{
+    "default_enconding": "UTF-8",
+    "default_line_ending": "UNIX",
+    "font_size": 30,
+    "tab_size": 4,
+    "translate_tabs_to_spaces": true
+}
