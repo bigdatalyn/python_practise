ff2a16fbede4410975320f4361b26727f554da67
bigdatalyn <88322511@qq.com>
2019-09-18 14:47:48 +0800

    update

--- 2019/Sep/Lesson03/01_Basic01.py
+++ 2019/Sep/Lesson03/01_Basic01.py
@@ -1,3 +1,30 @@
 #encoding: utf-8
 
-print(1/2)
+# n = 0
+# m = 1
+# while n <= 8:
+#     n = n + 1
+#     m = 1
+#     while m <= n:
+#         print(str(m) + '*' + str(n) + '=' + str(m*n) + ' ',end='')
+#         m = m + 1
+#     print('')
+
+import random
+
+guess = random.randint(0,100)
+count = 0
+print('Answer: ' + str(guess))
+while True:
+    g = input('Plz input a number: ')
+    g = int(g)
+    count += 1
+    if guess == g:
+        print('Write! You guess number is :' + str(g))
+        break
+    elif guess > g:
+        print('Wrong! You guess number is greater than ' + str(g))
+    elif guess < g:
+        print('Wrong! You guess number is smaller than ' + str(g))
+    if count >= 5:
+        break
